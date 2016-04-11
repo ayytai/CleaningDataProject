98b07de67b72c5e03ca1d52bbeea8512ea4d3296
Anju Tai <ayytai@gmail.com>
2016-04-10 20:59:57 -0600

    initial check-in

---
+++ run_analysis.R
@@ -0,0 +1,71 @@
+## Getting and Cleaning Data Course Project
+## By Anju Tai
+
+## Set data directories
+##dataDir <- "J:/Coursera/JHU Data Science/Data Cleaning/Assignment/UCI HAR Dataset"
+dataDir <- paste(getwd(), "UCI HAR Dataset", sep = "/")
+testDir <- paste(dataDir, "test", sep = "/")
+trainDir <- paste(dataDir, "train", sep = "/")
+
+## Set file names
+subjectTestFile <- paste(testDir, "subject_test.txt", sep = "/")
+xTestFile <- paste(testDir, "X_test.txt", sep = "/")
+yTestFile <- paste(testDir, "y_test.txt", sep = "/")
+
+subjectTrainFile <- paste(trainDir, "subject_train.txt", sep = "/")
+xTrainFile <- paste(trainDir, "X_train.txt", sep = "/")
+yTrainFile <- paste(trainDir, "y_train.txt", sep = "/")
+
+activityLabelsFile <- paste(dataDir, "activity_labels.txt", sep = "/")
+featuresFile <- paste(dataDir, "features.txt", sep = "/")
+
+tidyDatasetFile <- paste(dataDir, "tidy_data_set.txt", sep = "/")
+
+## Read activities and features
+activities  <- read.table(activityLabelsFile)
+names(activities) = c("id", "label")
+
+features <- read.table(featuresFile)
+names(features) = c("id", "label")
+
+## Read and combine test data sets
+testSubjects <- read.table(subjectTestFile)
+testX <- read.table(xTestFile, stringsAsFactors = FALSE)
+names(testX) <- features[, 2]
+testY <- read.table(yTestFile)
+testDataSet <- cbind(testSubjects, testY, testX)
+names(testDataSet)[1] <- "subject"
+names(testDataSet)[2] <- "activity"
+
+## Read and combine training data sets
+trainSubjects <- read.table(subjectTrainFile)
+trainX <- read.table(xTrainFile, stringsAsFactors = FALSE)
+names(trainX) <- features[, 2]
+trainY <- read.table(yTrainFile)
+trainDataSet <- cbind(trainSubjects, trainY, trainX)
+names(trainDataSet)[1] <- "subject"
+names(trainDataSet)[2] <- "activity"
+
+## 1. Merge the training and the test sets to create one data set.
+allData <- rbind(testDataSet, trainDataSet)
+
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+toBeSelected <- grep("mean\\(\\)|std\\(\\)", names(allData))
+columnIndices  <- c(1, 2, toBeSelected)
+extractedData <- allData[, columnIndices]
+
+## 3. Uses descriptive activity names to name the activities in the data set
+extractedData$activity <- activities[extractedData$activity, 2]
+
+## 4. Appropriately labels the data set with descriptive variable names.
+## *This is done already
+
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average 
+## of each variable for each activity and each subject.
+
+extractedDataGrouped <- group_by(extractedData, activity, subject)
+summarizedData <- summarise_each(extractedDataGrouped, funs(mean))
+
+## output tidy data set
+write.table(summarizedData, tidyDatasetFile, row.names = FALSE)
+
